9c803be060256ebbe6f7e844e3a3969ddfa7eb3f
dearzade@gmail.com <bilalrakhim@gmail.com>
2019-11-22 18:35:55 +0400

    sonluq

--- js/main.js
+++ js/main.js
@@ -1,10 +1,14 @@
+
 $(window).scroll(function(){
-    if($(window).scrollTop()>250){
-        $(".navbar").addClass("colorzilla")
-    }
-    else( 
-        $(".navbar").removeClass("colorzilla"))
+  if($(window).scrollTop()>250){
+      $(".navbar").addClass("colorzilla")
+  }
+  else( 
+      $(".navbar").removeClass("colorzilla"))
 }),
+
+
+
 $(".slider").slick({
     slidesToShow:4,
     infinity:true,
@@ -37,4 +41,37 @@ $(".slider").slick({
         // settings: "unslick"
         // instead of a settings object
       ]
+})
+$(".img-blog2").slick({
+  slidesToShow:1,
+  infinity:true,
+  autoplay:200,
+  responsive: [
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 3,
+          slidesToScroll: 3,
+          infinite: true,
+          dots: true
+        }
+      },
+      {
+        breakpoint: 600,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 2
+        }
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1
+        }
+      }
+      // You can unslick at a given breakpoint now by adding:
+      // settings: "unslick"
+      // instead of a settings object
+    ]
 })
